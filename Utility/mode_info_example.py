11d9e75f66a54056d2b309435e3cfd84cf596134
MonomCZ <vitek.monom@gmail.com>
2026-08-26 20:27:00 +0200

    testing gh shi

--- Utility/mode_info_example.py
+++ Utility/mode_info_example.py
@@ -1,8 +1,8 @@
 #each "mode" has a Name_info.py file which will tell the system how it works whot stuff can be changed etc
-
+#
 #it will be like this
 
-main_file_name=evil_twin.py #tells the system what mode to import
+main_file_name=evil_twin.py #tells the system what mode to importls ~/.ssh/
 
 start = run_eviltwin(ssid,captive_portal_type) #this start tell the system what will happen when you start the mode
 
