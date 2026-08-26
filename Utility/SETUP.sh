c6ec1dea8260075200362c26cca514135e5b9234
MonomCZ <vitek.monom@gmail.com>
2026-08-26 02:10:16 +0200

    added more reqs to setup.sh

--- Utility/SETUP.sh
+++ Utility/SETUP.sh
@@ -5,6 +5,22 @@ sudo apt upgrade -y
 sudo apt install -y python3-pip python3-venv python3-pil i2c-tools
 cd ~/MeowPiFour
 
+#sets up the python path for the project (i think so atleast)
+echo 'PYTHONPATH=/home/avsie/MeowPi-3' | sudo tee -a /etc/environment
+source /etc/environment
+#installs pip packages
+pip3 install --break-system-packages adafruit-blinka adafruit-circuitpython-ssd1306
+
 
 sudo raspi-config nonint do_i2c 0
 
+#fix for a rare issue i once had with ssh
+grep -qxF 'export TERM=xterm-256color' ~/.bashrc || echo 'export TERM=xterm-256color' >> ~/.bashrc
+
+cat << 'EOF'
+                           ╱|、
+                          (˚ˎ 。7
+                          |、˜〵        
+                          じしˍ,)ノ
+EOF
+echo "Setup complete :3"
