7fb701b56bd0de73160eec1ce13e1e19c1077109
MonomCZ <vitek.monom@gmail.com>
2026-08-26 19:50:33 +0200

    perhance working importing system

--- Functions/dynamic_mode_import.py
+++ Functions/dynamic_mode_import.py
@@ -6,12 +6,10 @@ modes_dir='Modes'
 modelist = [ item for item in os.listdir(modes_dir) if os.path.isdir(os.path.join(modes_dir, item)) ]
 #print(modelist)
 
-
+imported_modes = {}
 for mode in modelist:
     mode_info= mode + "_info"
     import_mode = f'Modes.{mode}.{mode_info}'
-    mode_info = __import__(import_mode, fromlist=[mode_info])
+    imported_modes[mode_info] = __import__(import_mode, fromlist=[mode_info])
 
-print(BadUsb_info.start)
-
-    
+print(imported_modes['BadUsb_info'].start)
