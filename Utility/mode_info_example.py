e0d93196db3471cab3b752c30fa79bb27ede4702
MonomCZ <vitek.monom@gmail.com>
2026-08-27 01:00:47 +0200

    typo

--- Utility/mode_info_example.py
+++ Utility/mode_info_example.py
@@ -4,7 +4,7 @@
 
 main_file_name=evil_twin.py #tells the system what file to import
 
-start = run_eviltwin(ssid,captive_portal_type) #this start tell the system what will happen when you start the mode
+start = run_eviltwin(ssid,captive_portal_type) #this start tells the system what will happen when you start the mode
 
 options = {ssid: ["Free WiFi", "McDonalds", "Starbucks"],
            captive_portal_type: ["Default", "McDonalds", "Starbucks"]} #this tells the system what options can be changed and what the options are
