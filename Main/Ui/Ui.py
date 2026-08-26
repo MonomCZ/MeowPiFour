51c88165c7c334823c16ad731241e8228474a2e7
MonomCZ <vitek.monom@gmail.com>
2026-08-26 18:28:21 +0200

    changed the ui to display temps

--- Main/Ui/Ui.py
+++ Main/Ui/Ui.py
@@ -1,6 +1,6 @@
 import Functions.oled_display as oled_display
 
 oled_display.clear()
-oled_display.display_text("Hello World!^This is a test.", 0)
+oled_display.display_temperature()
 oled_display.show()
 
