69cde5d52e9849fa479351e323fbcdeccadeb50b
MonomCZ <vitek.monom@gmail.com>
2026-08-26 19:53:23 +0200

    testing python basics lol

--- Functions/dynamic_mode_import.py
+++ Functions/dynamic_mode_import.py
@@ -12,4 +12,4 @@ for mode in modelist:
     import_mode = f'Modes.{mode}.{mode_info}'
     imported_modes[mode_info] = __import__(import_mode, fromlist=[mode_info])
 
-print(imported_modes['BadUsb_info'].start)
+print(imported_modes[0].start)
