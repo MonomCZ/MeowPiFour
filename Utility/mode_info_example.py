9fd8bd56362dbdbc48a883a4ea781cba986da02f
MonomCZ <vitek.monom@gmail.com>
2026-08-26 18:58:48 +0200

    testing putting folders in a list

---
+++ Utility/mode_info_example.py
@@ -0,0 +1,9 @@
+#each "mode" has a Name_info.py file which will tell the system how it works whot stuff can be changed etc
+
+#it will be like this
+
+start = run_eviltwin(ssid,captive_portal_type) #this start tell the system what will happen when you start the mode
+
+options = {ssid: ["Free WiFi", "McDonalds", "Starbucks"],
+           captive_portal_type: ["Default", "McDonalds", "Starbucks"]} #this tell the system what options can be changed and what the options are
+
