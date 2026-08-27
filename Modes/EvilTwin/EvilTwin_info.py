e5119dae476e1e32980b1b039265871200bcf791
MonomCZ <vitek.monom@gmail.com>
2026-08-27 02:08:08 +0200

    fixed again

--- Modes/EvilTwin/EvilTwin_info.py
+++ Modes/EvilTwin/EvilTwin_info.py
@@ -2,7 +2,7 @@ main_file_name = "evil_twin.py"
 start = 'run_eviltwin()'
 
 options = {
-    "presets": ["PRESET: FREE WIFI", "PRESET: STARBUCKS WIFI"],
+    "presets": ["FREE WIFI", "STARBUCKS WIFI"],
     'WLAN': ["TP_LINK_ADAPTER", "INTERNAL_WIFI_CHIP"]
 }
 
