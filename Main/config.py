3d59edbb021917dd67167efc8dcfaf5a7a60b67d
MonomCZ <vitek.monom@gmail.com>
2026-08-31 23:20:46 +0200

    why do you have to be so complicated ('reference to a random song idk the name of'

--- Main/config.py
+++ Main/config.py
@@ -1,5 +1,5 @@
-#maybe useless maybe not, not sure yet
-font = '/usr/share/fonts/truetype/hack/Hack-Bold.ttf'
+from PIL import ImageFont
+font = ImageFont.truetype('/usr/share/fonts/truetype/hack/Hack-Bold.ttf', 12)
 #list of fonts on the rpi atm
 # /usr/share/fonts/truetype/terminus/TerminusTTF-4.46.0.ttf
 # /usr/share/fonts/truetype/hack/Hack-BoldItalic.ttf
