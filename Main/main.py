a29e83b208ff43fd114d5cfde8ee632f6610b12e
MonomCZ <vitek.monom@gmail.com>
2026-08-31 23:22:13 +0200

    how can i not remember the changes that I MYSELF made

--- Main/main.py
+++ Main/main.py
@@ -4,9 +4,17 @@ import Main.config as config
 import Functions.oled_display as oled_display
 
 font = config.font
+#oled display shi
+WIDTH = 128
+HEIGHT = 64
+
+i2c = busio.I2C(board.SCL, board.SDA)
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
+oled.fill(0)
+oled.show()
 
 #import_modes_infos, imported_main_mode_files = Functions.dynamic_mode_import.import_modes()
 
 oled_display.clear()
 oled_display.display_text("Hello, World!", 0, font)
-oled_display.show()
+oled_display.show(oled)
