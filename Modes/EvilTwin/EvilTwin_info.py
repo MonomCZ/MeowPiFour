95dd55117a194165bcf6845f4f6d76f72adff510
dobruskyvojta-creator <dobruskyvojta@gmail.com>
2026-09-03 09:41:53 +0200

    fix: update WLAN option in selected_options to wlan0

--- Modes/EvilTwin/EvilTwin_info.py
+++ Modes/EvilTwin/EvilTwin_info.py
@@ -9,7 +9,7 @@ options = {
 #If no value is selected, the default value will be used.
 selected_options = {
     "SSID": "FREE WIFI",
-    'WLAN': "wlan1",
+    'WLAN': "wlan0", # DONT FORGET TO CHANGE IT TO WLAN 1
     "PORTAL": "free_wifi.html"
      
 
