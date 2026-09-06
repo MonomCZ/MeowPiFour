2fe5b7054bc4fdc95a334ebe8ff2723c2f9164b4
MonomCZ <vitek.monom@gmail.com>
2026-09-06 10:00:27 +0200

    seeing what sticks

--- Functions/ui.py
+++ Functions/ui.py
@@ -166,7 +166,7 @@ def list_modes(font):
     y = 0
     for mode in modelist:
         oled_display.display_text(mode, y, font)
-        y += 20
+        y += 2
         oled_display.show(oled)
         time.sleep(1)
         oled_display.clear()
