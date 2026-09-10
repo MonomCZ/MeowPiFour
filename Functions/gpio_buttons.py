b1279da2a32e4fa6d94dc2626e7f7d13d2c1de18
MonomCZ <vitek.monom@gmail.com>
2026-09-10 09:41:59 +0200

    added support for a third button

--- Functions/gpio_buttons.py
+++ Functions/gpio_buttons.py
@@ -4,8 +4,11 @@ import time
 
 btn1 = 22
 btn2 = 5
+btn3 = 6
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(btn2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(btn3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def button1():
     while True:
@@ -24,8 +27,17 @@ def button2():
         else:
             return False
 
+def button3():
+    while True:
+        if GPIO.input(btn3) == 1:
+            return True
+        else:
+            return False
+
 while True:
     if button1():
         print("Button 1 pressed")
     if button2():
         print("Button 2 pressed")
+    if button3():
+        print("Button 3 pressed")
