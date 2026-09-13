f86bdc8712ef85046dd5ab64d5d58b71f8caa388
Vojta <dobruskyvojta@gmail.com>
2026-09-13 16:19:40 +0200

    Test: on line 73/72 - Wlan1 dissconects and connets

--- Modes/EvilTwin/evil_twin.py
+++ Modes/EvilTwin/evil_twin.py
@@ -69,9 +69,9 @@ def configure_iptables():
 #This funkcion connects to the open wifi network that is available in the area. This is useful if you want to use the internet while running the Evil Twin attack.
 def connect_open_wifi():
     #HOME WIFI - Wlan1 feature: connects to your home wifi that was previously connected to, so you can still use the internet while running the Evil Twin attack.
-    cmd(["nmcli", "connection", "up", "$(nmcli -g GENERAL.CONNECTION device show wlan0)", "ifname", IFACE_INTERNET], ignore_error=True,)
-
-
+    #cmd(["nmcli", "connection", "up", "$(nmcli -g GENERAL.CONNECTION device show wlan0)", "ifname", IFACE_INTERNET], ignore_error=True,)
+    subprocess.run(f"nmcli connection up \"$(nmcli -g GENERAL.CONNECTION device show {IFACE_INTERNET})\" ifname {IFACE_INTERNET}", shell=True, ignore_error=True)
+    
     #If home wifi is not connected, it will try to connect to the strongest open wifi network available.
     cmd(["nmcli", "device", "wifi", "rescan", "ifname", IFACE_INTERNET], ignore_error=True)
     vystup = cmd(["nmcli", "--terse", "--fields", "SSID", "device", "wifi", "list", "ifname", IFACE_INTERNET, "--filter", "SECURITY.SECURITY==--"], ignore_error=True)
